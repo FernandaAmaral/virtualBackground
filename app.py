56d868da13912dae58fe07aedf9b5dd69019b4de
Fernanda Amaral <fernanda.amaral.melo@gmail.com>
2021-02-08 13:40:31 -0300

    code documentation

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-###############################################################################
-# Author: Fernanda Amaral Melo
-# Contact: fernanda.amaral.melo@gmail.com
-###############################################################################
+"""Virtual Background Interface
+
+This script uses the tkinter module to create a GUI for
+1: Selecting a desired virtual background image
+2: Taking a picture of the real background using webcam
+3. Controlling the background replacement algorith paramenters
+(binary image threshold and opening/closing kernel size)
+
+This script requires that `opencv and matplotlib` be installed within
+the Python environment you are running this script in.
+"""
 
 import tkinter as tk
 from tkinter import filedialog
@@ -24,47 +31,52 @@ class Application:
     def __init__(self, master=None):
         self.widget = tk.Frame(master)
         self.widget.pack()
-        self.choosePicBtn()
+        self._choose_pic_btn()
 
-    def changeState(self):
+    def _change_state(self):
+        """Manages the application state"""
         global appState
         appState += 1
-        if appState == 1:
+        if appState == 1:  # Take the real bg picture
             self.selectBtn.destroy()
-            self.takePicBtn()
-            self.takePicHandler()
-        if appState == 2:
+            self._take_pic_btn()
+            self._take_pic_handler()
+        if appState == 2:  # Replaces the webcam image bg by the virtual one
             self.webcam.destroy()
             self.takePicBtn.destroy()
             self.vbg = virtualBackground(bg=self.bg, virtualbg=self.virtualbg)
-            self.virtualBgView()
-            self.replaceBg()
+            self._virtual_bg_view()
+            self._replace_bg()
 
-    def choosePicBtn(self):
+    def _choose_pic_btn(self):
+        """Displays the GUI button for choosing the virtual background file"""
         self.selectBtn = tk.Button(
             self.widget,
             highlightbackground='#4197B2',
             text="Select a virtual background image",
-            command=self.choosePicBtnHandler,
+            command=self._choose_pic_handler,
         )
         self.selectBtn.pack()
 
-    def choosePicBtnHandler(self):
+    def _choose_pic_handler(self):
+        """Button handler for the virtual background choosen file"""
         path = filedialog.askopenfilename()
         self.virtualbg = cv2.imread(path)
-        self.changeState()
+        self._change_state()
 
-    def takePicBtn(self):
+    def _take_pic_btn(self):
+        """Displays the GUI button for taking the real background photo"""
         self.takePicBtn = tk.Button(
             self.widget,
             text='Take the real background photo',
             highlightbackground='#4197B2',
-            command=self.changeState)
+            command=self._change_state)
         self.takePicBtn.pack()
         self.webcam = tk.Label(self.widget)
         self.webcam.pack()
 
-    def takePicHandler(self):
+    def _take_pic_handler(self):
+        """Button handler for the real background taken photo"""
         _, frame = cap.read()
         self.bg = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(self.bg, cv2.COLOR_BGR2RGBA)
@@ -72,37 +84,76 @@ class Application:
         imgtk = ImageTk.PhotoImage(image=img)
         self.webcam.imgtk = imgtk
         self.webcam.configure(image=imgtk)
-        self.webcam.after(10, self.takePicHandler)
+        self.webcam.after(10, self._take_pic_handler)
 
-    def insertText(self, text):
+    def _insert_text(self, text):
+        """Creates a text widget on the GUI
+
+        Parameters
+        ----------
+        text : string
+            The text to be displayed
+        """
         txt = tk.Text(self.widget, width=10, height=0)
         txt.insert(tk.INSERT, text)
         txt.configure(state='disabled')
         txt.pack(side=tk.LEFT)
 
-    def virtualBgView(self):
+    def _create_slider(self, min, max):
+        """Creates a slider widget on the GUI
+
+        Parameters
+        ----------
+        min : int
+            The slider minimum value
+        max : int
+            The slider maximum value
+        """
+        return tk.Scale(self.widget, from_=min, to=max, orient='horizontal')
+
+    def _virtual_bg_view(self):
+        """Frame containing the webcam image with the virtual background
+        replacement and 3 sliders for controlling binary image threshold
+        and opening/closing kernel size
+        """
         self.plot = tk.Label(self.widget)
         self.plot.pack()
-        self.insertText('Threshold')
-        self.threshold_slider = tk.Scale(self.widget, from_=0, to=255, orient='horizontal')
+        self._insert_text('Threshold')
+        self.threshold_slider = self._create_slider(0, 255)
         self.threshold_slider.pack(side=tk.LEFT)
-        self.insertText('Opening')
-        self.opening_slider = tk.Scale(self.widget, from_=4, to=100, orient='horizontal')
+        self._insert_text('Opening')
+        self.opening_slider = self._create_slider(4, 100)
         self.opening_slider.pack(side=tk.LEFT)
-        self.insertText('Closing')
-        self.closing_slider = tk.Scale(self.widget, from_=4, to=100, orient='horizontal')
+        self._insert_text('Closing')
+        self.closing_slider = self._create_slider(4, 100)
         self.closing_slider.pack(side=tk.LEFT)
 
-    def replaceBg(self):
+    def _replace_bg(self):
+        """Frame containing the webcam image with the virtual background
+        replacement and 3 sliders for controlling binary image threshold
+        and opening/closing kernel size
+        """
+        # Takes the cwebcam frame
         _, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        new_pic = self.vbg.replaceBg(frame, self.threshold_slider.get(), self.opening_slider.get(), self.closing_slider.get())
-        new_pic = cv2.cvtColor(new_pic, cv2.COLOR_BGR2RGBA)
+
+        # Replace background
+        new_pic = self.vbg.replace_bg(
+            frame,
+            self.threshold_slider.get(),
+            self.opening_slider.get(),
+            self.closing_slider.get())
+
+        # Converts the image to the tkinter required format
         new_pic = Image.fromarray(new_pic)
         imgtk = ImageTk.PhotoImage(image=new_pic)
+
+        # Plot replaced bg image
         self.plot.imgtk = imgtk
         self.plot.configure(image=imgtk)
-        self.plot.after(10, self.replaceBg)
+
+        # Update camera image
+        self.plot.after(10, self._replace_bg)
 
 
 root = tk.Tk()
--- virtualBg.py
+++ virtualBg.py
@@ -1,7 +1,19 @@
-###############################################################################
-# Author: Fernanda Amaral Melo
-# Contact: fernanda.amaral.melo@gmail.com
-###############################################################################
+"""Virtual Background Interface
+
+This script uses the tkinter module to create a GUI for
+1: Selecting a desired virtual background image
+2: Taking a picture of the real background using webcam
+3. Controlling the background replacement algorith paramenters
+(binary image threshold and opening/closing kernel size)
+
+This script requires that `opencv and matplotlib` be installed within
+the Python environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+    * replace_bg - returns the picture with replaced background
+"""
+
 
 import cv2
 import numpy as np
@@ -17,31 +29,87 @@ class virtualBackground(object):
     def __init__(self, bg, virtualbg):
         self.bg = bg
         self.virtualbg = virtualbg
-        self._resizeImage()
+        self._resize_image()
 
-    def replaceBg(self, pic, threshold=THRESHOLD, n_opening=N_OPENING, n_closing=N_CLOSING):
+    def replace_bg(self, pic, threshold=THRESHOLD, n_opening=N_OPENING, n_closing=N_CLOSING):
+        """Replaces the real background of the image "pic" by a virtual background
+
+        Parameters
+        ----------
+        pic : image
+            The frame you want the background to be replaced
+        threshold : int
+            Threshold in pixels for image binarization after bg subtraction
+        n_opening: int
+            Size of opening kernel matrix for opening operation
+        n_closing: int
+            Size of opening kernel matrix for closing operation
+
+        Returns
+        -------
+        image
+            Picture with replaced background
+        """
+        # Saves the arguments parameters
         self.threshold = threshold
         self.n_opening = int(n_opening)
         self.n_closing = int(n_closing)
-        binary_pic = self._getBinaryPic(pic)
-        new_pic = pic * binary_pic
-        new_bg = self.virtualbg * (1 - binary_pic)
-        new_pic = new_bg + new_pic
-        return new_pic.astype('uint8')
 
-    def _resizeImage(self):
+        # Get the binary picture
+        binary_pic = self._get_binary_pic(pic)
+
+        # Removes the real background (background area is 0 in binary pic)
+        new_pic = pic * binary_pic
+
+        # Removes the foreground pixels from the virtual background
+        new_bg = self.virtualbg * (1 - binary_pic)
+
+        # Create the new image by summing the foreground with the virtual bg
+        new_pic = new_bg + new_pic
+        new_pic = new_pic.astype('uint8')
+        return cv2.cvtColor(new_pic, cv2.COLOR_BGR2RGBA)
+
+    def _resize_image(self):
+        """Resize the virtual bakground image if it doesn't fit the real
+        background shape"""
         dim = tuple(reversed(self.bg.shape[:-1]))
         if(self.virtualbg.shape != self.bg.shape):
             self.virtualbg = cv2.resize(self.virtualbg, dim)
 
-    def _getBinaryPic(self, pic):
+    def _get_binary_pic(self, pic):
+        """Subtracts the background from pic and uses the threshold parameter to
+        create a binary matrix containing the labels (0 for background and 1
+        for foreground)
+
+        Parameters
+        ----------
+        pic : image
+            The frame you want the background to be replaced
+
+        Returns
+        -------
+        np.matrix
+            Binary matrix
+        """
         binary_pic = pic - self.bg
         th, binary_pic = cv2.threshold(binary_pic, self.threshold, 255, cv2.THRESH_BINARY)
-        binary_pic = self.morphOperations(binary_pic)
+        binary_pic = self._morph_operations(binary_pic)
         binary_pic = binary_pic > self.threshold
         return binary_pic.astype(int)
 
-    def morphOperations(self, image):
+    def _morph_operations(self, image):
+        """Perform opening and closing operations on image for noise removal
+
+        Parameters
+        ----------
+        image : image
+            The noisy binary image
+
+        Returns
+        -------
+        image
+            Noise clean binary image
+        """
         kernel = np.ones((self.n_opening, self.n_opening))
         opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
         kernel = np.ones((self.n_closing, self.n_closing))
@@ -49,6 +117,12 @@ class virtualBackground(object):
         return closing
 
     def plot(self, image):
+        """Plot the image
+
+        Parameters
+        ----------
+        image : image
+        """
         plt.imshow(image)
         plt.show()
 
@@ -59,5 +133,5 @@ if __name__ == '__main__':
     virtualbg = cv2.imread('images/virtualbg.jpg')
 
     vbg = virtualBackground(bg, virtualbg)
-    new_pic = vbg.replaceBg(pic)
+    new_pic = vbg.replace_bg(pic)
     vbg.plot(new_pic)
